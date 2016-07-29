7f42a7c5bd7df43be3c0b57bb7837c0bc3aaff90
leon.sui <leon.sui@powervision.me>
2016-07-29 09:58:04 +0800

    用定时器接收串口，实现了串口高速收发

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,9 +8,11 @@
 #include <QMessageBox>
 #include <QtCore/QFile>
 #include <QtCore/QIODevice>
+#include <QFileDialog>
 #include <QDebug>
+#include <QTimer>
 
-#define RX_BUF_SIZE  1024*8
+#define RX_BUF_SIZE  1024*16
 #define STR_BUF_SIZE  RX_BUF_SIZE*1024
 char lData[RX_BUF_SIZE];
 char lStrBuf[STR_BUF_SIZE];
@@ -23,9 +25,20 @@ MainWindow::MainWindow(QWidget *parent) :
 //    lData = new char(RX_BUF_SIZE);
 //    lStrBuf = new char(STR_BUF_SIZE);
     memset(lStrBuf,0,STR_BUF_SIZE);
+    _ComIsOpen = false;
+    _DisplayTimer.setInterval(500);
+    _DisplayTimer.start();
+    connect(&_DisplayTimer,SIGNAL(timeout()),this,SLOT(DisplayTimeout()));
+
+    setWindowTitle("IMU Calibration Tool");
     ui->closeMyComBtn->setEnabled(false); //开始“关闭串口”按钮不可用
     ui->sendMsgBtn->setEnabled(false); //开始“发送数据”按钮不可用
-
+    this->setStyleSheet("background-color:rgb(200,200,200)");
+    ui->groupBox_2->setStyleSheet("color:black");
+    ui->groupBox_2->setStyleSheet("background-color:rgb(200,200,200)");
+    ui->textBrowser->setStyleSheet("background-color:rgb(240,240,240)");
+    ui->textBrowser_2->setStyleSheet("background-color:rgb(240,240,240)");
+    ui->IMU_ID_lineEdit->setStyleSheet("background-color:rgb(240,240,240)");
 }
 char tmp;
 MainWindow::~MainWindow()
@@ -59,7 +72,7 @@ bool rx_filter(char *rx_data,char *rx_buf,int rx_buf_len)
                                         // warnx("Find sencond Head");
                                         _rx_cmd_order = 2;
 
-                                        memset( rx_data , 0 , RX_BUF_SIZE );
+//                                        memset( rx_data , 0 , RX_BUF_SIZE );
                                         rx_data[0] = 0x00;
                                         rx_data[1] = 0x8A;
                                         _rx_cmd_data_addr = 2;
@@ -68,8 +81,8 @@ bool rx_filter(char *rx_data,char *rx_buf,int rx_buf_len)
                                         _rx_cmd_order = 0;
                                 }
                                 break;
-                        case 2://				rx_data[_rx_cmd_data_addr] = rx_buf[i];
-
+                        case 2:
+                                rx_data[_rx_cmd_data_addr] = rx_buf[i];
                                 if( ++_rx_cmd_data_addr == SUNCO_RX_CMD_SIZE ){
 
 //                                        if(rx_buf[i]==0x0A){//
@@ -93,50 +106,71 @@ bool rx_filter(char *rx_data,char *rx_buf,int rx_buf_len)
         }
         return false;
 }
-
+void MainWindow::DisplayTimeout()
+{
+    if(_ComIsOpen){
+        readMyCom();
+    }
+//    ui->textBrowser->append("Hello");
+}
 
 int  SentOrder = 0;
 void MainWindow::readMyCom() //读串口函数
 {
     static QByteArray gRecData="";
-    static char lRxCmd[64];
+//    static char lRxCmd[64];
+
 //    QByteArray temp = myCom->readAll();
 ////    //读取串口缓冲区的所有数据给临时变量temp
 //    char *lData = temp.data();
     memset(lData,0,RX_BUF_SIZE);
     int lCnt = myCom->read(lData,RX_BUF_SIZE);
-    if(lCnt>0){
-        if(rx_filter(lRxCmd,lData,lCnt)){
-            char lStr[256];
-            memset(lStr,0,sizeof(lStr));
-            sprintf(lStr,"AA:%8.6f",*(double *)rx_filter);
-            ui->textBrowser->setText(lStr);
-            memset(lRxCmd,0,sizeof(lRxCmd));
-        }
+
+    ui->textBrowser->append(QString("lCnt:%1").arg(lCnt));
+//    if(lCnt>0){
+//        if(rx_filter(lRxCmd,lData,lCnt)){
+//            static int lRxCmdCnt = 0;
+//            static int lRxCmdPrintCnt = 0;
+//            static int lRxCmdTotalCnt = 0;
+//            ++lRxCmdTotalCnt;
+//            if( ++lRxCmdCnt>200 ){
+//                lRxCmdCnt = 0;
+//                char lStr[256];
+//                memset(lStr,0,sizeof(lStr));
+//                sprintf(lStr,"Order:%d | Total:%8d",lRxCmdPrintCnt++,lRxCmdTotalCnt);
+////                ui->textBrowser->setText(lStr);
+////                ui->textBrowser->append("Hello");
+//            }
+//            memset(lRxCmd,0,sizeof(lRxCmd));
+//        }
 
 
 
 
 
-//        sprintf(lStrBuf,"%d>>%d:%x,%x,%x\r\n",SentOrder++,lCnt,*(int *)lData,*(int *)(lData+4),*(int *)(lData+8));
-//        memcpy(lStrBuf+lStrBuf_addr,lData,lCnt);
-        gRecData.append(lData);
-        lStrBuf_addr += lCnt;
-//        ui->textBrowser->insertPlainText(lStrBuf);
-        if(lStrBuf_addr>100000){
-            ui->textBrowser->setText(gRecData);
-//            ui->textBrowser->moveCursor(QTextCursor::End);
-            lStrBuf_addr=0;
-        }
+////        sprintf(lStrBuf,"%d>>%d:%x,%x,%x\r\n",SentOrder++,lCnt,*(int *)lData,*(int *)(lData+4),*(int *)(lData+8));
+////        memcpy(lStrBuf+lStrBuf_addr,lData,lCnt);
+//        gRecData.append(lData);
+//        lStrBuf_addr += lCnt;
+////        ui->textBrowser->insertPlainText(lStrBuf);
+//        if(lStrBuf_addr>100000){
+//            ui->textBrowser->setText(gRecData);
+////            ui->textBrowser->moveCursor(QTextCursor::End);
+//            lStrBuf_addr=0;
+//        }
 
-    }
+//    }
 }
 void MainWindow::on_openMyComBtn_clicked()
 {
     QString portName = ui->portNameComboBox->currentText(); //获取串口名
-    myCom = new Win_QextSerialPort(portName,QextSerialBase::EventDriven);
+    myCom = new Win_QextSerialPort("\\\\.\\"+portName,QextSerialBase::EventDriven);
     //定义串口对象，并传递参数，在构造函数里对其进行初始化
-    myCom ->open(QIODevice::ReadWrite); //打开串口
+    if(!myCom ->open(QIODevice::ReadWrite)){//打开串口
+        ui->textBrowser->append("Com Open Failed!");
+        return;
+    }
+    setWindowTitle(ui->IMU_ID_lineEdit->text()+":"+ui->portNameComboBox->currentText());
     if(ui->baudRateComboBox->currentText()==tr("9600")) //根据组合框内容对串口进行设置
     myCom->setBaudRate(BAUD9600);
     else if(ui->baudRateComboBox->currentText()==tr("115200"))
@@ -161,7 +195,7 @@ void MainWindow::on_openMyComBtn_clicked()
     //设置停止位
     myCom->setFlowControl(FLOW_OFF); //设置数据流控制，我们使用无数据流控制的默认设置
     myCom->setTimeout(500); //设置延时
-    connect(myCom,SIGNAL(readyRead()),this,SLOT(readMyCom()));
+//    connect(myCom,SIGNAL(readyRead()),this,SLOT(readMyCom()));
     //信号和槽函数关联，当串口缓冲区有数据时，进行读串口操作
     ui->openMyComBtn->setEnabled(false); //打开串口后“打开串口”按钮不可用
     ui->closeMyComBtn->setEnabled(true); //打开串口后“关闭串口”按钮可用
@@ -171,9 +205,12 @@ void MainWindow::on_openMyComBtn_clicked()
     ui->parityComboBox->setEnabled(false);
     ui->stopBitsComboBox->setEnabled(false);
     ui->portNameComboBox->setEnabled(false);
+
+    _ComIsOpen = true;
 }
 void MainWindow::on_closeMyComBtn_clicked()
 {
+    _ComIsOpen = false;
     myCom->close();
     ui->openMyComBtn->setEnabled(true); //关闭串口后“打开串口”按钮可用
     ui->closeMyComBtn->setEnabled(false); //关闭串口后“关闭串口”按钮不可用
@@ -192,47 +229,6 @@ void MainWindow::on_sendMsgBtn_clicked()
     char lSentData[64];
     lSentData[0] = 0xAE;
     lSentData[1] = 0xA2;
-
-
-
-
-//    -2.358333333	3.889319444	-0.83775
-//    -16.34295833	0.028242796	0.097135871
-//    -0.030534328	16.33298333	0.029176391
-//    -0.101929836	-0.029802727	-16.31939583
-
-
-//    lSentData[2] = 0x40;
-//    *(float *)(lSentData+3) = -2.358333333f  ;//3#Gyro
-//    *(float *)(lSentData+7) = 3.889319444f   ;
-//    *(float *)(lSentData+11) = -0.83775f    ;
-
-
-//    *(float *)(lSentData+15) = 16.34295833f   ;//
-//    *(float *)(lSentData+19) = -0.028242796f   ;
-//    *(float *)(lSentData+23) = 0.097135871f    ;
-
-//    *(float *)(lSentData+27) = 0.030534328f   ;
-//    *(float *)(lSentData+31) = 16.33298333f   ;//
-//    *(float *)(lSentData+35) = -0.029176391f   ;
-
-//    *(float *)(lSentData+39) = -0.101929836f;
-//    *(float *)(lSentData+43) = 0.029802727f;
-//    *(float *)(lSentData+47) = 16.31939583f;//
-
-
-
-
-
-
-//    -6.001736111	29.8108	76.7962
-//    -25.09258333	30.94791667	74.09870833
-
-//    4079.780167	-115.2742917	418.561
-//    125.2491667	4094.3785	-120.3297917
-//    -414.4640417	130.2855417	4075.508792
-
-
 
     lSentData[2] = 0x30;
     *(float *)(lSentData+3) = -6.001736111f;//3#ACCEL
@@ -301,7 +297,7 @@ void MainWindow::on_TranBtn_clicked()
     ui->textBrowser->moveCursor(QTextCursor::End);
 }
 
-void MainWindow::on_CleanScreamBtn_clicked()
+void MainWindow::on_CleanLeftScreamBtn_clicked()
 {
     ui->textBrowser->clear();
 }
@@ -309,6 +305,7 @@ void MainWindow::on_CleanScreamBtn_clicked()
 void MainWindow::on_testBtn_clicked()
 {
     ui->textBrowser->insertPlainText("111111111111111111111111\r\n");
+    ui->textBrowser_2->insertPlainText("111111111111111111111111\r\n");
 }
 
 float lFileData[8][3];
@@ -317,8 +314,7 @@ float lFileData[8][3];
 
 void MainWindow::on_ReadFileBtn_clicked()
 {
-    QFile file(ui->FileName_lineEdit->text());
-//    QFile file("D:\Qt\test\111.cpp");
+    QFile file(_ParamsFileName);
     if(file.open(QIODevice::ReadOnly|QIODevice::Text)){
         QString lReadBuf;
         QString lStrValue;
@@ -361,8 +357,19 @@ void MainWindow::on_ReadFileBtn_clicked()
 //        ui->textBrowser->insertPlainText(lReadBuf);
 
     }else{
-        ui->textBrowser->insertPlainText(ui->FileName_lineEdit->text());
+        ui->textBrowser->insertPlainText(_ParamsFileName);
          ui->textBrowser->insertPlainText("File Open failed!\r\n");
     }
     file.close();
+}
+
+void MainWindow::on_CleanRightScreamBtn_clicked()
+{
+        ui->textBrowser_2->clear();
+}
+
+void MainWindow::on_ChooseFileBtn_clicked()
+{
+    _ParamsFileName = QFileDialog::getOpenFileName(this,tr("open file"),"",tr("Allfile(*.*);;datFile(*.dat)"));
+    ui->textBrowser_2->append(_ParamsFileName);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "win_qextserialport.h"
+#include <QTimer>
 
 namespace Ui {
     class MainWindow;
@@ -19,15 +20,21 @@ public:
 private:
     Ui::MainWindow *ui;
     Win_QextSerialPort *myCom;
+    QString  _ParamsFileName;
+    QTimer  _DisplayTimer;
+    bool    _ComIsOpen;
 private slots:
+    void on_ChooseFileBtn_clicked();
+    void on_CleanRightScreamBtn_clicked();
     void on_ReadFileBtn_clicked();
     void on_testBtn_clicked();
-    void on_CleanScreamBtn_clicked();
+    void on_CleanLeftScreamBtn_clicked();
     void on_TranBtn_clicked();
     void on_sendMsgBtn_clicked();
     void on_closeMyComBtn_clicked();
     void on_openMyComBtn_clicked();
     void readMyCom();
+    void DisplayTimeout();
 };
 
 #endif // MAINWINDOW_H
